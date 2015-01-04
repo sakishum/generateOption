f6eac08af3d04bd0787cd3a22234acf89cc0dfb5
sakishum <sakishum11118@gmail.com>
2015-01-04 19:56:42 +0800

    code review.

--- tests/hello.py
+++ tests/hello.py
@@ -1,18 +1,1 @@
-'''
-Author:  UserName 
-Email:  UserEmail
-Time: 2014,07,03 18:36:11
-Version: 
-Description:
-
-'''
-
-'''
-Author:  UserName 
-Email:  UserEmail
-Time: 2014,07,03 18:36:10
-Version: 
-Description:
-
-'''
 
